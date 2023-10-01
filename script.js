9da53ba46276d3e4be181629d7aa58e65de803c6
Rohan <64094065+Rohan-ingle@users.noreply.github.com>
2023-10-01 18:43:16 +0530

    V2.4
    Major update
    -Added spotify embeds
    -Added banners
    -Added a mystery song

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function scrollVideo() {
     const yPos = -(window.scrollY * scrollSpeed);
     video.style.transform = `translate3d(0px, ${yPos}px, 0px)`;
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const playButton = document.getElementById("playButton");
+    const audioPlayer = document.getElementById("audioPlayer");
+
+    playButton.addEventListener("click", function () {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            playButton.textContent = "Pause";
+        } else {
+            audioPlayer.pause();
+            playButton.textContent = "Play Song";
+        }
+    });
+});
